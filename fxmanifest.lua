b9dec8a89658f6b30aef578fbc5cf7c599c9fa4a
Zesk García Rodrígues <62112491+ZeskooV@users.noreply.github.com>
2022-05-29 12:52:31 +0200

    Simple Loading Screen Creation & Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+game 'gta5'
+fx_version 'adamant'
+author 'ZsK'
+files {
+    'nui/css/*.css',
+    -- 'nui/js/*.js',
+    'nui/*.html',
+    'nui/*.webm',
+    'nui/*.mp4'
+}
+loadscreen 'nui/load.html'
+loadscreen_manual_shutdown 'yes'
